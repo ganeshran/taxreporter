0e47b11f92a1bbd74f524e84c9517a8be0651a08
Ganesh Ranganathan <ganesh.ranganathan.85@gmail.com>
2015-10-14 01:38:34 +0530

    Adding core contracts

---
+++ TaxReporter/TaxReporter.Core/Entities/IInvoiceEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxReporter.Core.Entities
+{
+    public interface IInvoiceEntry
+    {
+        public int InvoiceNumber { get; set; }
+
+        public IClient Client { get; set; }
+
+
+    }
+}
