261bf24e2ea500f690c47f5180f27b7d0098153e
Ganesh Ranganathan <ganesh.ranganathan.85@gmail.com>
2015-10-20 16:51:58 +0530

    adding a factory class for creating the structure map

---
+++ TaxReporter.Core/Factories/ITaxFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TaxReporter.Core.Enums;
+using TaxReporter.Core.Taxes;
+
+namespace TaxReporter.Core.Factories
+{
+    public interface ITaxFactory
+    {
+        ITaxDue GetTaxInstance(TaxTypes type);
+    }
+}
---
+++ TaxReporter.Services/Factories/TaxFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TaxReporter.Core.Enums;
+using TaxReporter.Core.Factories;
+using TaxReporter.Core.Taxes;
+
+namespace TaxReporter.Services.Factories
+{
+    public class TaxFactory: ITaxFactory
+    {
+        public ITaxDue GetTaxInstance(TaxTypes type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
